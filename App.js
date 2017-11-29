d465f397f3e402c4bd7b92132a3be7fd19b798b6
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-11-29 21:20:37 +0100

    Set up state port, counter and RN UI
    In Elm program count number of seconds sunce start.
    
    On evry update send the count via port called state.
    
    In RN app subscribe to the port (inside componentDidMount) and set
    component's state to whatever comes out of the port.
    
    Display the count in render method.

--- App.js
+++ App.js
@@ -5,12 +5,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import Elm from './lib/main';
 
 let worker = Elm.Main.worker();
-export default class App extends React.Component {
 
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counr: 0 };
+  }
+  componentDidMount() {
+    worker.ports.state.subscribe(count => this.setState({ count }));
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app, pal!</Text>
+        <Text>{this.state.count}</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
@@ -23,6 +31,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
--- Main.elm
+++ Main.elm
@@ -1,10 +1,15 @@
 port module Main exposing (..)
 
 import Platform
+import Time exposing (every, second)
 
 
 type alias Model =
-    ()
+    Int
+
+
+type Msg
+    = Increment
 
 
 main =
@@ -16,15 +21,22 @@ main =
 
 
 init =
-    Debug.log "Elm init" () ! []
+    Debug.log "Elm init" 1 ! []
 
 
 subscriptions model =
-    Sub.none
+    always Increment
+        |> Time.every second
 
 
 update msg model =
-    () ! []
+    case msg of
+        Increment ->
+            let
+                next =
+                    Debug.log "next state" (model + 1)
+            in
+                next ! [ state next ]
 
 
 port state : Model -> Cmd msg
